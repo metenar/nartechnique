f364b5aa9398a02747dcec8f746fe3212376c1ba
Berna <narberna@gmail.com>
2024-09-09 14:28:07 -0700

    prepearing hosting

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8801",
+  baseURL: "http://nartechnique.com:8801",
   withCredentials: true,
 });
 export default apiRequest;
