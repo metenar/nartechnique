95d0263b94d95f07948523670c05c836645c0403
Berna <narberna@gmail.com>
2024-09-09 14:27:40 -0700

    prepearing host

--- api/index.js
+++ api/index.js
@@ -6,11 +6,15 @@ import projectsRoute from "./routes/projects.js";
 import usersRoute from "./routes/user.js";
 import authRoute from "./routes/auth.js";
 import cors from "cors";
+import path from "path";
+import url, { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8800;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
@@ -27,6 +31,11 @@ app.use("/projects", projectsRoute);
 app.use("/users", usersRoute);
 app.use("/auth", authRoute);
 
+app.use(express.static(path.join(__dirname, "../")));
+
+app.get("*", req, (res) => {
+  res.send(path.join(__dirname, "../", "index.html"));
+});
 app.listen(PORT, () => {
   connect();
   console.log(`connected to backend server! using ${PORT} port`);
