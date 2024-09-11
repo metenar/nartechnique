011dcf448ef42f81674e82827199f38cd63f8f04
Berna <narberna@gmail.com>
2024-09-11 09:20:49 -0700

    test

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://nartechnique.com:8801",
+  baseURL: "http://nartechnique.onrender.com:8801",
   withCredentials: true,
 });
 export default apiRequest;
