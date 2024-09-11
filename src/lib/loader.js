b192596a700503d5639d04defc91a6cde47615e6
Berna <narberna@gmail.com>
2024-09-11 09:59:30 -0700

    test4

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -1,6 +1,7 @@
 import apiRequest from "./apiRequest";
 
 export const reviewLoader = async ({ request, params }) => {
+  console.log("loaderdayim");
   const res = await apiRequest("/reviews");
   const sortedResponse = await res.data.sort(
     (a, b) => new Date(b.date) - new Date(a.date)
