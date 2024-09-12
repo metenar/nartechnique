b21785c8d3a116a2c8b1608840500de041fd45b3
Berna <narberna@gmail.com>
2024-09-12 09:07:13 -0700

    test7

--- api/index.js
+++ api/index.js
@@ -30,6 +30,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+
   next();
 });
 
