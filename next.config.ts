d98b2905c64b121d412657a2c266ebecbd6836e3
Berna <narberna@gmail.com>
2026-05-20 14:01:18 -0700

    Next logo removal

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.4.28', 'http://192.168.4.28:3000'],
+  devIndicators: false,
 };
 
 export default nextConfig;
